95ed9136b85ec5790a839afc2b1a1f667af8c077
Rowan Chapman <rchapman83@users.noreply.github.com>
2017-10-03 13:55:51 +1100

    added req and startup

---
+++ run.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Entry point to bring up the web app
+
+from os import environ
+import subprocess
+
+c = environ.get('APP_CONFIG')
+a = environ.get('APP_MODULE')
+
+# Run Gunicorn to serve the app
+print('START')
+subprocess.call(['gunicorn', '-c', c, a])
+print('...FIN')
+
+# Run the Flask app server
+# from app import application
+# application.run(host='0.0.0.0', port=8080)
