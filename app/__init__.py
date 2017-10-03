e79584a43a363390b1af172886eeb95f576aa59d
Rowan Chapman <rchapman83@users.noreply.github.com>
2017-10-03 14:05:22 +1100

    routes added

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Flask web app
+
+# Flask mod imports
+from flask import Flask
+
+# Construct application
+sticksWeb = Flask(__name__.split('.')[0], static_folder='static', template_folder='templates')
+sticksWeb.config.from_envvar('FLASK_SETTINGS')
+
+
+# Import all the views for this web app
+from . import routes
---
+++ app/routes.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# All the views here
+
+# Import app via circular imports
+from . import sticksWeb
+# Import flask stuff
+from flask import Flask, render_template, request, url_for, send_from_directory
+
+@sticksWeb.route('/')
+def entry():
+        return render_template('index.html')
+
+@sticksWeb.route('/robots.txt')
+def robots_static():
+  return send_from_directory(application.static_folder, request.path[1:])
+
+@sticksWeb.route('/sitemap.xml')
+def sitemap_static():
+  return send_from_directory(application.static_folder, request.path[1:])
+
+@sticksWeb.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+@sticksWeb.errorhandler(500)
+def server_error(error):
+    return render_template('500.html'), 500
