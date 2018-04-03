9eb4869ec54dddae608785eb8d8da9ed6b440532
rchapman83 <rchapman83@gmail.com>
2018-04-03 23:53:29 +0000

    update basics

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 # -*- config:utf-8 -*-
 
 import multiprocessing
+import os
+
 
 bind = '0.0.0.0:8080'
 name = 'sticks-clothing'
 backlog = 2048
 max_requests = 1000
-proc_name = 'sticks-web'
+proc_name = os.environ.get('PROJECT_NAME')
 workers = int((multiprocessing.cpu_count() * 2) + 1)
 threads = int(multiprocessing.cpu_count()+1)
 daemon = False
@@ -21,4 +23,3 @@ accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 forwarded_allow_ips = '*'
-# secure_scheme_headers = {'X-FORWARDED-PROTOCOL': 'ssl', 'X-FORWARDED-PROTO': 'https', 'X-FORWARDED-SSL': 'on'}
